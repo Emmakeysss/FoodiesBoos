45e6044b7c9d960cab5092e312588f9f4bfe1eae
Emanuel gonzalez <emmanuelgonzalezmanu11@gmail.com>
2023-07-25 21:34:58 -0300

    dadafasf

--- js/index.js
+++ js/index.js
@@ -1,5 +1,79 @@
+//section carrito 
+
+class item {
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+let tarjeta = document.querySelectorAll("#card");
+
+const productos = [];
+const carrito = {};
+
+tarjeta.forEach(function (card){
+
+     // section info cards
+
+    let nombreDelProducto = card.querySelector("#title").textContent;
+    let precioDelProducto = card.querySelector("#price").textContent;
+    let precio = parseFloat(precioDelProducto.substring(1));
+    let producto = new item(nombreDelProducto, precio);
+    productos.push(producto);
+
+
+    // section btn
+
+    let boton = card.querySelector("#btn-orden");
+    boton.addEventListener("click", agregarAlCarrito);
+
+    function agregarAlCarrito() {
+        if (carrito[nombreDelProducto]) {
+            // Si el producto ya está en el carrito, aumenta la cantidad
+            carrito[nombreDelProducto].cantidad++;
+        } else {
+            // Si es la primera vez que se agrega el producto, crea una entrada en el carrito
+            carrito[nombreDelProducto] = {
+                producto: producto,
+                cantidad: 1
+            };
+        }
+        
+        console.log(carrito);
+    }
+
+    let btnCompra = document.getElementById("btn-compra");
+    btnCompra.addEventListener("click", compraComfirmada);
+
+    function compraComfirmada (){
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+    }
+
+    let btnStorages = document.getElementById("btn-storages");
+    btnStorages.addEventListener("click", traerStorages);
+
+    function traerStorages (){
+        let carritoDelStorage = JSON.parse(localStorage.getItem("carrito"));
+        console.log(carritoDelStorage);
+    }
+});
+
+
+
+
+
+
+  
+
+
+
 // active navbar 
+
 let nav = document.querySelector(".navegation-wrap");
+
+
+
 window.onscroll = function () {
     if(document.documentElement.scrollTop > 20){
         nav.classList.add("scroll-on")
@@ -7,9 +81,6 @@ window.onscroll = function () {
         nav.classList.remove("scroll-on")
     }
 }
-
-
-
 
 // nav hide 
 let navBar = document.querySelectorAll(".nav-link");
